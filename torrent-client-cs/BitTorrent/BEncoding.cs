638de789240e3665e5f2a008051d9ac59e84b3a1
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-04-30 17:09:13 -0300

    feat: decode methods boilerplate implementation

--- torrent-client-cs/BitTorrent/BEncoding.cs
+++ torrent-client-cs/BitTorrent/BEncoding.cs
@@ -6,29 +6,52 @@ using System.IO;
 
 namespace BitTorrent;
 
-public static class BEncoding {
-    private static byte DictionaryStart  = Encoding.UTF8.GetBytes("d")[0]; // 100
-    private static byte DictionaryEnd    = Encoding.UTF8.GetBytes("e")[0]; // 101
-    private static byte ListStart        = Encoding.UTF8.GetBytes("l")[0]; // 108
-    private static byte ListEnd          = Encoding.UTF8.GetBytes("e")[0]; // 101
-    private static byte NumberStart      = Encoding.UTF8.GetBytes("i")[0]; // 105
-    private static byte NumberEnd        = Encoding.UTF8.GetBytes("e")[0]; // 101
+public static class BEncoding
+{
+    private static byte DictionaryStart = Encoding.UTF8.GetBytes("d")[0]; // 100
+    private static byte DictionaryEnd = Encoding.UTF8.GetBytes("e")[0]; // 101
+    private static byte ListStart = Encoding.UTF8.GetBytes("l")[0]; // 108
+    private static byte ListEnd = Encoding.UTF8.GetBytes("e")[0]; // 101
+    private static byte NumberStart = Encoding.UTF8.GetBytes("i")[0]; // 105
+    private static byte NumberEnd = Encoding.UTF8.GetBytes("e")[0]; // 101
     private static byte ByteArrayDivider = Encoding.UTF8.GetBytes(":")[0]; // 58
 
-    public static object Decode(byte[] bytes) {
+    public static object Decode(byte[] bytes)
+    {
         IEnumerator<byte> enumerator = ((IEnumerable<byte>)bytes).GetEnumerator();
         enumerator.MoveNext();
         return DecodeNextObject(enumerator);
     }
-    
-    private static object DecodeNextObject(IEnumerator<byte> enumerator) {
-        if(enumerator.Current == DictionaryStart)
-            return null; // It will be implemented the DecodeDictionary(enumerator) method
-        if(enumerator.Current == ListStart)
-            return null; // It will be implemented the DecodeList(enumerator) method
-        if(enumerator.Current == NumberStart)
-            return null; // It will be implemented the DecodeNumber(enumerator) method
-        return null; // It will be implemented the DecodeByteArray(enumerator) method
+
+    private static object DecodeNextObject(IEnumerator<byte> enumerator)
+    {
+        if (enumerator.Current == DictionaryStart)
+            return DecodeDictionary(enumerator);
+        if (enumerator.Current == ListStart)
+            return DecodeList(enumerator);
+        if (enumerator.Current == NumberStart)
+            return DecodeNumber(enumerator);
+        return DecodeByteArray(enumerator);
+    }
+
+    private static object DecodeDictionary(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException("this method was not implemented");
+    }
+
+    private static object DecodeList(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException("this method was not implemented");
+    }
+
+    private static object DecodeNumber(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException("this method was not implemented");
+    }
+
+    private static object DecodeByteArray(IEnumerator<byte> enumerator)
+    {
+        throw new NotImplementedException("this method was not implemented");
     }
 }
 
