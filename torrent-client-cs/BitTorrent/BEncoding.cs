ba115832b7e17ed6312e534896a6a83c78ba023a
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-05-03 21:05:55 -0300

    feat: encode methods implementation

--- torrent-client-cs/BitTorrent/BEncoding.cs
+++ torrent-client-cs/BitTorrent/BEncoding.cs
@@ -111,6 +111,76 @@ public static class BEncoding
     }
     #endregion
 
+    #region Encode
+    public static byte[] Encode(object obj)
+    {
+        MemoryStream buffer = new MemoryStream();
+        EncodeNextObject(buffer, obj);
+        return buffer.ToArray();
+    }
+
+    public static void EncodeFile(object obj, string path)
+    {
+        File.WriteAllBytes(path, Encode(obj));
+    }
+
+    private static void EncodeNextObject(MemoryStream buffer, object obj)
+    {
+        if (obj is byte[])
+            EncodeByteArray(buffer, (byte[])obj);
+        else if (obj is string)
+            EncodeString(buffer, (string)obj);
+        else if (obj is long)
+            EncodeNumber(buffer, (long)obj);
+        else if (obj.GetType() == typeof(List<object>))
+            EncodeList(buffer, (List<object>)obj);
+        else if (obj.GetType() == typeof(Dictionary<string, object>))
+            EncodeDictionary(buffer, (Dictionary<string, object>)obj);
+        else
+            throw new Exception("unable to encode type " + obj.GetType());
+    }
+
+    private static void EncodeNumber(MemoryStream buffer, long input)
+    {
+        buffer.Append(NumberStart);
+        buffer.Append(Encoding.UTF8.GetBytes(Convert.ToString(input)));
+        buffer.Append(NumberEnd);
+    }
+
+    private static void EncodeByteArray(MemoryStream buffer, byte[] body)
+    {
+        buffer.Append(Encoding.UTF8.GetBytes(Convert.ToString(body.Length)));
+        buffer.Append(ByteArrayDivider);
+        buffer.Append(body);
+    }
+
+    private static void EncodeString(MemoryStream buffer, string input)
+    {
+        EncodeByteArray(buffer, Encoding.UTF8.GetBytes(input));
+    }
+
+    private static void EncodeList(MemoryStream buffer, List<object> input)
+    {
+        buffer.Append(ListStart);
+        foreach (var item in input)
+            EncodeNextObject(buffer, item);
+        buffer.Append(ListEnd);
+    }
+
+    private static void EncodeDictionary(MemoryStream buffer, Dictionary<string, object> input)
+    {
+        buffer.Append(DictionaryStart);
+        var sortedKeys = input.Keys.ToList().OrderBy(x => BitConverter.ToString(Encoding.UTF8.GetBytes(x)));
+        foreach (var key in sortedKeys)
+        {
+            EncodeString(buffer, key);
+            EncodeNextObject(buffer, input[key]);
+        }
+        buffer.Append(DictionaryEnd);
+    }
+    #endregion
+}
+
 public static class MemoryStreamExtensions
 {
     public static void Append(this MemoryStream stream, byte value)
